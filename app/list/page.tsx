c8f526079431ace9581de8f347f5fee2e3e0b6bb
devwoodie <ehddl453@naver.com>
2023-10-23 15:50:30 +0900

    feat: item list button handler add

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
-import food from "/public/food0.png";
+"use client"
+
+import {useState} from "react";
 
 export default function Page() {
 
     let items: Array<string> = ['Tomato', 'Pasta', 'Coconut'];
+    const [num, setNum] = useState<number[]>([0, 0, 0]);
+
+    const handlePlus = (idx: number) => {
+        let copy: number[] = [...num];
+        copy[idx]++;
+        return setNum(copy);
+    }
+    const handleMinus = (idx: number) => {
+        let copy: number[] = [...num];
+        if(copy[idx] > 0){
+            copy[idx]--;
+            return setNum(copy);
+        }else {
+            return 0
+        }
+    }
 
     return (
         <div>
@@ -12,6 +29,9 @@ export default function Page() {
                 <div className="food" key={idx}>
                     <img src={`/food${idx}.png`} alt="img" className="food-img" />
                     <h4>{item} $40</h4>
+                    <span>{num[idx]}</span>
+                    <button onClick={() => handlePlus(idx)}>+</button>
+                    <button onClick={() => handleMinus(idx)}>-</button>
                 </div>
             ))}
         </div>
